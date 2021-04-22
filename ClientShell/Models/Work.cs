0ca9ae769ea1cba3932093fee12ed2a2eb545b01
ProMix0 <misha.zavertkin@mail.ru>
2021-04-22 14:18:58 +0700

    Work show

---
+++ ClientShell/Models/Work.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Drawing;
+
+namespace ClientShell
+{
+    public class WorkInfo
+    {
+        public string Name { get; }
+        public Bitmap Icon { get; }
+        public int Cost { get; }
+        public string Description { get; }
+        public byte[] AssemblyBytes { get; }
+
+        private Assembly assembly;
+        public void Run()
+        {
+            assembly??= Assembly.Load(AssemblyBytes);
+
+            assembly.EntryPoint.Invoke(null, null);
+        }
+
+    }
+}
---
+++ ClientShell/UserControls/Main/WorkList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientShell
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkList.xaml
+    /// </summary>
+    public partial class WorkList : UserControl
+    {
+        public static DependencyProperty SelectedProperty = DependencyProperty.Register(nameof(Selected), typeof(Control), typeof(WorkList));
+
+
+        public Control Selected { get; }
+        public ObservableCollection<WorkInfo> WorkItems { get; } = new ObservableCollection<WorkInfo>();
+
+        public WorkList()
+        {
+            InitializeComponent();
+        }
+    }
+}
